9d065ae8a52f0d3fca24f5dbb5c77a1ffebdf4a0
JustinFourie1993 <>
2023-11-23 10:36:31 +0000

    added NoteList and NoteDetail views

--- notes/models.py
+++ notes/models.py
@@ -4,9 +4,9 @@ from tasks.models import Task
 
 
 class Note(models.Model):
-    task = models.ForeignKey(
-        Task, on_delete=models.CASCADE, related_name='notes')
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -15,4 +15,6 @@ class Note(models.Model):
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"Note for {self.task.title}: {self.content}"
+        return self.content
+
+    
--- notes/serializers.py
+++ notes/serializers.py
@@ -7,16 +7,16 @@ class NoteSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    task = serializers.ReadOnlyField(source='task.id')
-
-    class Meta:
-        model = Note
-        fields = ['id', 'task', 'owner', 'created_at', 'updated_at', 'content']
-        read_only_fields = ['created_at', 'updated_at', 'owner', 'is_owner']
-
+    
     def get_is_owner(self, obj):
         return obj.owner == self.context['request'].user
 
-    def create(self, validated_data):
-        user = self.context['request'].user
-        return Note.objects.create(owner=user, **validated_data)
+    class Meta:
+        model = Note
+        fields = ['id', 'task', 'owner', 'created_at',
+                  'updated_at', 'content', 'profile_id', 'is_owner']
+
+
+class NoteDetailSerializer(NoteSerializer):
+    
+    task = serializers.ReadOnlyField(source='task.id')

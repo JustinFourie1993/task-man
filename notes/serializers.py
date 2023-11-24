7c30d3e5953ebe6a066f79a49b7150d648a92039
JustinFourie1993 <>
2023-11-24 15:07:00 +0000

    Added pagination to api

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 from .models import Note, Task
-from django.contrib.auth.models import User
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
 class NoteSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
     
     def get_is_owner(self, obj):
         return obj.owner == self.context['request'].user
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Note

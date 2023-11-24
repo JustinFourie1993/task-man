4a4ce67f56756ad63b57097cfc6433c1af1b90b1
JustinFourie1993 <>
2023-11-24 15:35:11 +0000

    0

--- task_man/settings.py
+++ task_man/settings.py
@@ -63,8 +63,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'DEV' in os.environ
+# DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-justinfourie1993-task-ma-vgchaze7bh.us2.codeanyapp.com', 'https://task-man-cec7dd2494da.herokuapp.com']
@@ -111,9 +111,9 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
-     CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-     ]
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
         r"^https://.*\.gitpod\.io$",
@@ -156,7 +156,6 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-    
 
 
 # Password validation

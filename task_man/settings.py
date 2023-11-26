c550443cf74976ef9172a9d5bc9795128ff514c1
JustinFourie1993 <>
2023-11-26 16:48:42 +0000

    change settings to run react app inside django app

--- task_man/settings.py
+++ task_man/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import re
+
 
 if os.path.exists('env.py'):
     import env
@@ -65,10 +65,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
-    '8000-justinfourie1993-task-ma-vgchaze7bh.us2.codeanyapp.com', os.environ.get('ALLOWED_HOST')]
+    os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
@@ -111,16 +111,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
+    CORS_ALLOWED_ORIGINs = [
         os.environ.get('CLIENT_ORIGIN')
-    ]
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^([^.]+)', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True

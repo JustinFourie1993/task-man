c554cd020b3e719ddcce2f88f74551e5893c29ee
JustinFourie1993 <>
2023-11-28 12:30:08 +0000

    added routing for home, signin and signup page

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/logo.jpg'
-import styles from '../styles/NavBar.module.css'
+import logo from '../assets/logo.jpg';
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 const NavBar = () => {
     return (
         <container>
             <Navbar className={styles.NavBar} expand="md" fixed="top">
-                <Navbar.Brand><img src={logo} alt="logo" height="45" /></Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand><img src={logo} alt="logo" height="45" /></Navbar.Brand></NavLink>
                 <Navbar.Toggle className={styles.dropdown} aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link ><i className="fa-solid fa-house-user"></i>Home</Nav.Link>
-                        <Nav.Link ><i className="fa-solid fa-right-to-bracket"></i>Sign In</Nav.Link>
-                        <Nav.Link ><i className="fa-solid fa-pen-to-square"></i>Sign Up</Nav.Link>
-
+                        <NavLink exact to="/"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}>
+                            <i className="fa-solid fa-house-user">
+                            </i>Home</NavLink>
+                        <NavLink to="/signin"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active} >
+                            <i className="fa-solid fa-right-to-bracket">
+                            </i>Sign In</NavLink>
+                        <NavLink to="/signup"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active} >
+                            <i className="fa-solid fa-pen-to-square">
+                            </i>Sign Up</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>

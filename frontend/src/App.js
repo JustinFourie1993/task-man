a2f071a77f8c616bc4d59ea591179933f862387c
JustinFourie1993 <>
2023-12-09 09:36:36 +0000

    added taskpage.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import TaskCreateForm from './tasks/TaskCreateForm';
+import TaskPage from './tasks/TaskPage';
 
 
 
@@ -20,6 +21,7 @@ function App() {
                             <Route exact path="/signin" render={() => <SignInForm />} />
                             <Route exact path="/signup" render={() => <SignUpForm />} />
                             <Route exact path="/tasks/create" render={() => <TaskCreateForm />} />
+                            <Route exact path="/tasks/:id" render={() => <TaskPage />} />
                             <Route render={() => <p>Page not found!</p>} />
                         </Switch>
                     </Container>

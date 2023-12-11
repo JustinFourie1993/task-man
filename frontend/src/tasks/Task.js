bb9f2a3edbbae7e5cb7ea3ab77aee15510ab2391
JustinFourie1993 <>
2023-12-11 16:52:30 +0000

    added jsx to display overdue days if overdue

--- frontend/src/tasks/Task.js
+++ frontend/src/tasks/Task.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
 import styles from "../styles/Task.module.css";
+import btnStyles from "../styles/Button.module.css";
 
 const Task = (props) => {
     const {
@@ -24,29 +26,49 @@ const Task = (props) => {
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner || owners?.includes(currentUser?.username);
 
-    const calculateOverdueDays = (dueDate) => {
-        const due = new Date(dueDate);
+    const calculateOverdueDays = () => {
+        if (!overdue) return 0;
+        const due = new Date(due_date);
         const now = new Date();
-        const diffTime = Math.max(due - now, 0);
+        const diffTime = Math.abs(now - due);
         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
         return diffDays;
     };
 
-    return (
+    return is_owner ? (
         <Card className={styles.Task}>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>{content}</Card.Text>
-                <strong>Category:</strong> {category} <br />
-                <strong>Due Date:</strong> {due_date} <br />
-                {overdue && (
-                    <span className={styles.Overdue}>
-                        Overdue by {calculateOverdueDays(due_date)} days
-                    </span>
+                {is_owner && taskPage && (
+                    <>
+                        <strong>Category:</strong> {category} <br />
+                        <strong>Due Date:</strong> {due_date} <br />
+                        {overdue && (
+                            <span className={styles.Overdue}>
+                                Overdue by {calculateOverdueDays()} days
+                            </span>
+                        )}
+                        <strong>Priority:</strong> {priority} <br />
+                        <strong>Status:</strong> {state} <br />
+                    </>
                 )}
-                <strong>Priority:</strong> {priority} <br />
-                <strong>Status:</strong> {state} <br />
-                {is_owner && taskPage && "..."}
+                <div className={styles.Comment}>
+                    <Link to={`/tasks/${id}`}>
+                        <Button className={btnStyles.Button}>
+                            Leave a note
+                        </Button>
+                    </Link>
+                </div>
+            </Card.Body>
+        </Card>
+    ) : (
+        <Card className={styles.Task}>
+            <Card.Body>
+                <Card.Title>Restricted Access</Card.Title>
+                <Card.Text>
+                    You do not have permission to view the details of this task.
+                </Card.Text>
             </Card.Body>
         </Card>
     );

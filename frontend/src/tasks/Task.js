07e5bd1a21078b40fe85d8596892c069c5174f10
JustinFourie1993 <>
2023-12-11 14:23:25 +0000

    added calculateOverduedays function to task.js

--- frontend/src/tasks/Task.js
+++ frontend/src/tasks/Task.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { Card } from 'react-bootstrap';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
-import styles from "../styles/Task.module.css"
+import styles from "../styles/Task.module.css";
 
 const Task = (props) => {
-    const{
+    const {
         category,
         content,
         created_at,
@@ -20,22 +20,32 @@ const Task = (props) => {
         profile_id,
         profile_image,
         taskPage,
-    } = props
+    } = props;
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner || owners?.includes(currentUser?.username);
+
+    const calculateOverdueDays = (dueDate) => {
+        const due = new Date(dueDate);
+        const now = new Date();
+        const diffTime = Math.max(due - now, 0);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return diffDays;
+    };
+
     return (
         <Card className={styles.Task}>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                    <Card.Text>{content}</Card.Text>
-                    <strong>Category:</strong> {category} <br />
-                    <strong>Due Date:</strong> {due_date} <br />
-                    <strong>Overdue:</strong> {overdue} <br />
-                    <strong>Priority:</strong> {priority} <br />
-                    <strong>Status:</strong> {state} <br />
-                    {overdue && <span className={styles.Overdue}>Overdue</span>}
-                </Card.Text>
+                <Card.Text>{content}</Card.Text>
+                <strong>Category:</strong> {category} <br />
+                <strong>Due Date:</strong> {due_date} <br />
+                {overdue && (
+                    <span className={styles.Overdue}>
+                        Overdue by {calculateOverdueDays(due_date)} days
+                    </span>
+                )}
+                <strong>Priority:</strong> {priority} <br />
+                <strong>Status:</strong> {state} <br />
                 {is_owner && taskPage && "..."}
             </Card.Body>
         </Card>

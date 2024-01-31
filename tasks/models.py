1e959d3ea051637619f88b2a6786b9055d3a05be
JustinFourie1993 <>
2024-01-31 16:45:20 +0000

    added field validation to task model fields

--- tasks/models.py
+++ tasks/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 from cloudinary.models import CloudinaryField
 
 
 class Task(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasks')
     owners = models.ManyToManyField(User, related_name='owned_tasks')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255, blank=True)
-    content = models.TextField(blank=True)
+    title = models.CharField(max_length=255, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
     due_date = models.DateTimeField(null=True, blank=True)
     overdue = models.BooleanField(default=False)
+
     PRIORITY_CHOICES = [
         ('LOW', 'Low'),
         ('MEDIUM', 'Medium'),
@@ -20,7 +23,9 @@ class Task(models.Model):
     priority = models.CharField(
         max_length=6,
         choices=PRIORITY_CHOICES,
-        default='LOW',)
+        default='LOW',
+    )
+
     CATEGORY_CHOICES = [
         ('WORK', 'Work'),
         ('PERSONAL', 'Personal'),
@@ -34,7 +39,11 @@ class Task(models.Model):
         ('SOCIAL', 'Social'),
     ]
     category = models.CharField(
-        max_length=100, choices=CATEGORY_CHOICES, blank=True)
+        max_length=100,
+        choices=CATEGORY_CHOICES,
+        blank=True,
+    )
+
     STATE_CHOICES = [
         ('OPEN', 'Open'),
         ('IN_PROGRESS', 'In Progress'),
@@ -42,10 +51,20 @@ class Task(models.Model):
         ('CLOSED', 'Closed'),
     ]
     state = models.CharField(
-        max_length=20, choices=STATE_CHOICES, default='OPEN')
+        max_length=20,
+        choices=STATE_CHOICES,
+        default='OPEN',
+    )
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.id} {self.title}"
+        return f"{self.title} (ID: {self.id})"
+
+    def save(self, *args, **kwargs):
+        if self.due_date and self.due_date < timezone.now():
+            raise ValueError("Due date cannot be in the past.")
+        super(Task, self).save(*args, **kwargs)
+
+
